d221c2ee39580da2e7f6e73a936c54056c787d48
Julia Deng <juliaxdeng@gmail.com>
2021-03-12 16:14:31 -0800

    removed topx and edited csv files

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def parse_preferences(csv_path):
 	""" 
 	Takes in path to csv file
@@ -13,34 +12,10 @@ def parse_preferences(csv_path):
 	for i in range(len(df)):
 		mentees = []
 		for j in range(1, len(df.columns)):
-			if (not pd.isnull(df.iloc[i][j])):
+			if ((not pd.isnull(df.iloc[i][j])) & (df.iloc[i][j] not in mentees)):	# no duplicates
 				mentees.append(df.iloc[i][j])
 		mentor_preferences[df.iloc[i][0]] = mentees
-
 	return mentor_preferences
-
-def parse_extra_preferences(csv_path):
-	""" 
-	Takes in path to csv file
-	Returns dictionary of mentor / mentee to their top 3 preferences
-	With non-top 3 preferences, 
-	"""	
-	df = pd.read_csv(csv_path, sep=',')
-
-	mentor_preferences = {}
-	extra_mentor_preferences = {}
-
-	for i in range(len(df)):
-		mentees = []
-		for j in range(1, 4):		# only go thru top 3 preferences
-			if (not pd.isnull(df.iloc[i][j])):
-				mentees.append(df.iloc[i][j])
-		mentor_preferences[df.iloc[i][0]] = mentees
-		for k in range(4, len(df.columns)):		# go thru the extra preferences
-			if (not pd.isnull(df.iloc[i][k])):
-				mentees.append(df.iloc[i][k])
-		extra_mentor_preferences[df.iloc[i][0]] = mentees
-	return mentor_preferences, extra_mentor_preferences	
 
 def parse_num_mentees(csv_path):
 	df = pd.read_csv(csv_path, sep=',')
